3df9651e9b5b02f83ca33e2572cc32c97302ac71
Vadym <71566934+vodemkka@users.noreply.github.com>
2024-03-07 20:56:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+let canSpin = true;
+
+function spinWheel() {
+    if (!canSpin) return;
+
+    let wheel = document.querySelector(".inner-wheel");
+    let arrow = document.querySelector(".arrow");
+
+    let fixedPrizeIndex = 4; // Номер приза, который будет постоянным
+
+    canSpin = false;
+
+    let deg = Math.floor(Math.random() * 360) + 360 * 5; // Генерируем случайный угол
+
+    wheel.style.transition = "all 6s ease-out";
+
+    wheel.style.transform = `rotate(${deg}deg)`;
+
+    // Вращаем стрелку
+    arrow.style.transition = "all 6s ease-out";
+    arrow.style.transform = `rotate(${deg}deg)`;
+
+    // По завершению анимации
+    setTimeout(() => {
+        canSpin = true;
+        showModal(fixedPrizeIndex);
+    }, 6000);
+}
+
+function showModal(prizeIndex) {
+    let modal = document.getElementById("modal");
+    let prizeInfo = document.querySelector(".prize-info");
+    let prizeImage = document.querySelector(".prize-image");
+    let prizeName = document.querySelector(".prize-name");
+    let prizesList = document.querySelectorAll(".prizes-list li");
+
+    prizeImage.src = prizesList[prizeIndex].querySelector("img").src;
+    prizeName.textContent = prizesList[prizeIndex].textContent;
+
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    let modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
+
+function claimPrize() {
+    // Добавьте здесь логику для забора приза
+    // Например, отправка запроса на сервер или что-то подобное
+
+    closeModal();
+}
